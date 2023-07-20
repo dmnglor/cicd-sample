202307d44f3b09d3f4d350e9b7425c46c8fa507f
dmnglore <35909263+dmnglor@users.noreply.github.com>
2023-07-20 13:05:19 +0530

    first commit

---
+++ src/main/java/com/devops/sample/cicdsample/CicdsampleApplication.java
@@ -0,0 +1,13 @@
+package com.devops.sample.cicdsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdsampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/sample/cicdsample/CicdsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.sample.cicdsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
